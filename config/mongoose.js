35f2eb286cc78b94be40e2ef882a5b4baf4a1f79
Kcih4518 <kcih4518@gmail.com>
2021-07-29 20:01:15 +0800

    <feat>[AC-S2-3-W4-Short-URL]: Create mongoose module
    1. Disable deprecation warnings; useNewUrlParser, useUnifiedTopology, useCreateIndex

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/short-url'
+// Setting mongoose
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+// 1. Get database connection status
+const db = mongoose.connection
+
+// 2. Connection exception handling
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 3. Connection successfully handling
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
