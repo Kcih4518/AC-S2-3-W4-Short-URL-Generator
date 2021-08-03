0467571d4281c5076d21588f1230208386876b71
Kcih4518 <kcih4518@gmail.com>
2021-08-03 14:51:21 +0800

    <feat>[AC-S2-3-W4-Short-URL]: Add Create feature : Generate a set of shortURL and store it in the database with originURL
    1. Use req.body to get originURL
    2. Query the existence of information in the database through originURL
    3. If it exists, use the original shortURL, if it does not exist, generate a new set of shortURL

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const base62URL = require('../../tools/base62URL')
+const URL = require('../../models/urls')
+const PORT = process.env.PORT || 3000
+const urlProtocolAndServer = process.env.APP_URL || `http://localhost:${PORT}/`
 
 // Read : View the url input page
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+// Create: Add new short url
+router.post('/', async (req, res) => {
+  const originURL = req.body.originURL
+  const shortURLCheck = await URL.find({ originURL }).lean()
+
+  // Check the originURL, if the Database already exists, return the first shortURL
+  if (shortURLCheck.length) {
+    const shortURL = shortURLCheck[0].shortURL
+    return res.render('index', { originURL, shortURL, urlProtocolAndServer })
+  }
+
+  // Obtain a 5-character string, which is coded in base 62 by 5 arrays of 0~62 numbers
+  const shortURL = base62URL()
+
+  return URL.create({ originURL, shortURL })
+    .then(() => {
+      res.render('index', { originURL, shortURL, urlProtocolAndServer })
+    })
+    .catch((error) => console.log(error))
 })
 
 module.exports = router
